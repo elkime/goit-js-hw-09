26c6e4fdd8d6102b5cab83e0bcb39936e9112f80
Elkin Mejía <111328546+elkime@users.noreply.github.com>
2023-10-02 22:28:16 -0500

    tercer commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,4 @@
-import flatpickr from "flatpickr";
-// Importación adicional de estilos
-import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const initDelayInput = document.getElementsByName('delay')[0];
 const delayStepInput = document.getElementsByName('step')[0];
@@ -20,46 +18,3 @@ function checkInputs() {
     submit.disabled = true;
   }
 }
-
-initDelayInput.addEventListener('input', checkInputs);
-delayStepInput.addEventListener('input', checkInputs);
-amountInput.addEventListener('input', checkInputs);
-
-submit.addEventListener('click', event => {
-  event.preventDefault();
-
-  const initDelay = parseInt(initDelayInput.value);
-  const delayStep = parseInt(delayStepInput.value);
-  const amount = parseInt(amountInput.value);
-
-  let counter = 0;
-  let position = 1;
-  let delay = initDelay;
-  const timerId = setInterval(() => {
-    const shouldResolve = Math.random() > 0.3;
-
-    new Promise((resolve, reject) => {
-      if (shouldResolve) {
-        resolve(`promise ${position} in ${delay}ms`);
-      } else {
-        reject(` promise ${position} in ${delay}ms`);
-      }
-    }).then(
-      value => {
-        Notify.success(`✅ Fulfilled  ${value}`);
-      },
-      error => {
-        Notify.failure(`❌ Rejected ${error}`);
-      }
-    );
-
-    counter++;
-    position = position += 1;
-    delay = delay += delayStep;
-    if (counter === amount) {
-      clearInterval(timerId);
-      form.reset();
-      submit.disabled = true;
-    }
-  }, delayStep + initDelay);
-});
