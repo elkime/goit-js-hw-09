a5e36e1b239bebecc9b26b4d308fd1a6b3b8697e
Elkin Mejía <111328546+elkime@users.noreply.github.com>
2023-10-04 15:00:25 -0500

    finish task 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const start = document.querySelector('[data-start]');
-    const stop = document.querySelector('[data-stop]');
-    const body = document.querySelector('body');
-    let timerId = null;
-    start.classList.add('btn');
-    stop.classList.add('btn');
-  
-    function getRandomHexColor() {
-      return `#${Math.floor(Math.random() * 16777215)
-        .toString(16)
-        .padStart(6, '0')}`;
-    }
-  
-    start.addEventListener('click', () => {
-      timerId = setInterval(() => {
-        body.style.backgroundColor = getRandomHexColor();
-      }, 1000);
-      start.disabled = true;
-    });
-  
-    stop.addEventListener('click', () => {
-      clearInterval(timerId);
-      start.disabled = false;
-    });
+  const start = document.querySelector('[data-start]');
+  const stop = document.querySelector('[data-stop]');
+  const body = document.querySelector('body');
+  let timerId = null;
+  start.classList.add('btn');
+  stop.classList.add('btn');
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')}`;
+  }
+
+  start.addEventListener('click', () => {
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    start.disabled = true;
   });
-  
+
+  stop.addEventListener('click', () => {
+    clearInterval(timerId);
+    start.disabled = false;
+  });
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -18,3 +18,46 @@ function checkInputs() {
     submit.disabled = true;
   }
 }
+
+initDelayInput.addEventListener('input', checkInputs);
+delayStepInput.addEventListener('input', checkInputs);
+amountInput.addEventListener('input', checkInputs);
+
+submit.addEventListener('click', event => {
+  event.preventDefault();
+
+  const initDelay = parseInt(initDelayInput.value);
+  const delayStep = parseInt(delayStepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  let counter = 0;
+  let position = 1;
+  let delay = initDelay;
+  const timerId = setInterval(() => {
+    const shouldResolve = Math.random() > 0.3;
+
+    new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve(`promise ${position} in ${delay}ms`);
+      } else {
+        reject(` promise ${position} in ${delay}ms`);
+      }
+    }).then(
+      value => {
+        Notify.success(`✅ Fulfilled  ${value}`);
+      },
+      error => {
+        Notify.failure(`❌ Rejected ${error}`);
+      }
+    );
+
+    counter++;
+    position = position += 1;
+    delay = delay += delayStep;
+    if (counter === amount) {
+      clearInterval(timerId);
+      form.reset();
+      submit.disabled = true;
+    }
+  }, delayStep + initDelay);
+});
